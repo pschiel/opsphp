7b63ff94e714bf924f388fc1d9d83cf6d71f662e
Patrick Schiel <p.schiel@gmail.com>
2018-06-20 22:22:12 +0200

    cleanup

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -5,11 +5,6 @@
 	<meta charset="utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-	<?= $this->Html->css('/css/bootstrap.min.css') ?>
-	<?= $this->Html->css('/css/styles.css') ?>
-	<?= $this->Html->script('/js/jquery.min.js') ?>
-	<?= $this->Html->script('/js/bootstrap.min.js') ?>
-	<?= $this->Html->script('/js/script.js') ?>
 </head>
 <body>
 
